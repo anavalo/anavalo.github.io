aaff771b8390f4971351840619bd66cbd92f7bf4
Tasos Tsournos <48796551+anavalo@users.noreply.github.com>
2023-01-30 18:00:10 +0200

    exp build fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxtjs/color-mode'],
+  colorMode: {
+    preference: 'system', // default value of $colorMode.preference
+    fallback: 'light', // fallback value if not system preference found
+    hid: 'nuxt-color-mode-script',
+    globalName: '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    classPrefix: '',
+    classSuffix: '-mode',
+    storageKey: 'nuxt-color-mode',
+  },
   css: ['@/assets/global.scss'],
   vite: {
     css: {
@@ -13,17 +24,5 @@ export default defineNuxtConfig({
 
   typescript: {
     strict: true,
-  },
-
-  modules: ['@nuxtjs/color-mode'],
-  colorMode: {
-    preference: 'system', // default value of $colorMode.preference
-    fallback: 'light', // fallback value if not system preference found
-    hid: 'nuxt-color-mode-script',
-    globalName: '__NUXT_COLOR_MODE__',
-    componentName: 'ColorScheme',
-    classPrefix: '',
-    classSuffix: '-mode',
-    storageKey: 'nuxt-color-mode',
   },
 });
