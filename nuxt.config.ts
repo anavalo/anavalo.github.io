784d1a8645e365e7e16f81eba243c0dbb011e618
Tasos Tsournos <tasos@moveo.ai>
2023-01-31 20:57:36 +0200

    pre-release

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/color-mode'],
+  css: ['@/assets/global.scss'],
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: '@import "@/assets/global.scss";',
+          additionalData: '@use "@/assets/variables.scss" as *;',
         },
       },
     },
