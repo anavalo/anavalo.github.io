791acc7045c3301034eb4e6eaea71013e2d316b8
Tasos Tsournos <tasos@moveo.ai>
2023-01-31 19:06:54 +0200

    pre-release

--- components/Navbar/Navbar.vue
+++ components/Navbar/Navbar.vue
@@ -1,25 +1,23 @@
+<script setup>
+const colorMode = useColorMode();
+</script>
+
 <template>
   <div class="wrapper">
     <div>
-      <select v-model="$colorMode.preference">
+      <select v-model="colorMode.preference">
         <option value="system">System</option>
         <option value="light">Light</option>
         <option value="dark">Dark</option>
       </select>
     </div>
-    <span class="link">about me</span>
+    <span class="link">hello</span>
     <span class="link">blog</span>
     <span class="link">projects</span>
   </div>
 </template>
 
-<script setup>
-import { useColorMode } from '@nuxtjs/color-mode';
-const colorMode = useColorMode();
-</script>
-
 <style lang="scss">
-@import '@/assets/variables.scss';
 .wrapper {
   display: flex;
   gap: $space-16;
