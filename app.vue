c453762cf55a11f613cc4f25d072347b4b3eaac9
Tasos Tsournos <tasos@moveo.ai>
2023-02-01 20:49:12 +0200

    seo in header

--- app.vue
+++ app.vue
@@ -1,3 +1,13 @@
 <template>
   <NuxtLayout> <NuxtPage /> </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+useServerSeoMeta({
+  title: 'avavalo.com',
+  ogTitle: 'anavalo.com',
+  description: 'Tasos codes, writes and sometimes runs',
+  ogDescription: 'Tasos codes, writes and sometimes runs',
+  ogImage: 'https://anavalo.com/tasos.jpeg',
+});
+</script>
