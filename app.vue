13090c73a8e610f47a41961d6c7ae688a956f0ea
Tasos Tsournos <tasos@moveo.ai>
2023-02-01 23:34:52 +0200

    feat: card component wrapper

--- app.vue
+++ app.vue
@@ -16,3 +16,13 @@ useServerSeoMeta({
 .kgb {
 }
 </style>
+
+<script setup lang="ts">
+useServerSeoMeta({
+  title: 'avavalo.com',
+  ogTitle: 'anavalo.com',
+  description: 'Tasos codes, writes and sometimes runs',
+  ogDescription: 'Tasos codes, writes and sometimes runs',
+  ogImage: 'https://anavalo.com/tasos.jpeg',
+});
+</script>
