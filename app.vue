e1290df22781307cd5ebcb175b1110f39b60a6dc
Tasos Tsournos <tasos@moveo.ai>
2023-02-07 23:41:14 +0200

    feat: ga

--- app.vue
+++ app.vue
@@ -12,4 +12,23 @@ useServerSeoMeta({
   ogDescription: 'Tasos codes, writes and sometimes runs',
   viewport: { maximumScale: 1, minimumScale: 1, width: 'device-width' },
 });
+
+useHead({
+  script: [
+    {
+      src: 'https://www.googletagmanager.com/gtag/js?id=G-FRJFJ0K0N9',
+      async: true,
+    },
+    {
+      innerHTML: `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-FRJFJ0K0N9');
+      `,
+      type: 'text/javascript',
+    },
+  ],
+});
 </script>
--- plugins/ga.js
+++
@@ -1,9 +0,0 @@
-import { defineNuxtPlugin } from '#app';
-import VueGtag, { trackRouter } from 'vue-gtag';
-
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(VueGtag, {
-    property: { id: 'G-FRJFJ0K0N9' },
-  });
-  trackRouter(useRouter());
-});
