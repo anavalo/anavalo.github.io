fba6b09b1b20912190919d075a3222be042590e2
Tasos Tsournos <tasos@moveo.ai>
2024-01-23 14:48:04 +0200

    fix: gtm

--- app.vue
+++ app.vue
@@ -29,6 +29,24 @@ useHead({
       `,
       type: 'text/javascript',
     },
+    {
+      innerHTML: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-MN8S6MC');
+          `,
+      type: 'text/javascript',
+    },
+  ],
+  noscript: [
+    {
+      innerHTML: `
+      <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MN8S6MC"
+      height="0" width="0" style="display:none;visibility:hidden"></iframe>
+      `,
+    },
   ],
 });
 </script>
